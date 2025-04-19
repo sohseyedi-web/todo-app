885c909a312aedbb3ebd4717b6212442a4c43946
sohseyedi <sohseyedi@gmail.com>
2025-04-19 14:19:34 +0330

    add cursor

--- src/components/common/CategoryFilter.tsx
+++ src/components/common/CategoryFilter.tsx
@@ -15,7 +15,7 @@ const CategoryFilter = ({ value, onChange, options }: CategoryFilterProps) => {
       <select
         value={value}
         onChange={onChange}
-        className="w-full px-1 py-2 rounded-xl border border-zinc-700 bg-[#212121] text-zinc-100 focus:outline-none"
+        className="w-full px-1 py-2 cursor-pointer rounded-xl border border-zinc-700 bg-[#212121] text-zinc-100 focus:outline-none"
       >
         {options.map((option, index) => (
           <option key={index} value={option.value}>
